54913a27fae74a16c97a3642bea8ed2d0cf1c500
k025g1031 <k025g1031@g.neec.ac.jp>
2026-09-01 11:18:15 +0900

    1

--- main.cpp
+++ main.cpp
@@ -1,91 +1,33 @@
+#include <condition_variable>
 #include <iostream>
-#include <list>
-#include <cstdio>
-#include <cstring>
+#include <mutex>
+#include <thread>
 
+std::mutex mutex;
+std::condition_variable condition;
+int nextThread = 1;
 
-int main(){
-using namespace std;
+void PrintThread(int number) {
+    std::unique_lock<std::mutex> lock(mutex);
 
-std::list<const char*> yamanote;
+    
+    condition.wait(lock, [number] { return nextThread == number; });
 
-yamanote.push_back("Tokyo");
-yamanote.push_back("Kanda");
-yamanote.push_back("Akihabara");
-yamanote.push_back("Okachimachi");
-yamanote.push_back("Ueno");
-yamanote.push_back("Uguisudani");
-yamanote.push_back("Nippori");
-yamanote.push_back("Tabata");
-yamanote.push_back("Komagome");
-yamanote.push_back("Sugamo");
-yamanote.push_back("Otsuka");
-yamanote.push_back("Ikebukuro");
-yamanote.push_back("Mejiro");
-yamanote.push_back("Takadanobaba");
-yamanote.push_back("Shin-Okubo");
-yamanote.push_back("Shinjuku");
-yamanote.push_back("Yoyogi");
-yamanote.push_back("Harajuku");
-yamanote.push_back("Shibuya");
-yamanote.push_back("Ebisu");
-yamanote.push_back("Meguro");
-yamanote.push_back("Gotanda");
-yamanote.push_back("Osaki");
-yamanote.push_back("Shinagawa");
-yamanote.push_back("Tamachi");
-yamanote.push_back("Hamamatsucho");
-yamanote.push_back("Shimbashi");
-yamanote.push_back("Yurakucho");
+    std::cout << "thread " << number << '\n';
 
-printf("1970\n");
-for (auto it = yamanote.begin();
-    it != yamanote.end();
-    ++it)
-{
-    printf("%s\n", *it);
+    ++nextThread;
+    lock.unlock();
+    condition.notify_all();
 }
 
-for (auto it = yamanote.begin();
-    it != yamanote.end();
-    ++it)
-{
-    if (strcmp(*it, "Tabata") == 0)
-    {
-        yamanote.insert(it, "Nishi-Nippori");
-        break;
-    }
+int main() {
+    std::thread thread1(PrintThread, 1);
+    std::thread thread2(PrintThread, 2);
+    std::thread thread3(PrintThread, 3);
+
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    return 0;
 }
-printf("2019\n");
-
-for (auto it = yamanote.begin();
-    it != yamanote.end();
-    ++it)
-{
-    printf("%s\n", *it);
-}
-
-for (auto it = yamanote.begin();
-    it != yamanote.end();
-    ++it)
-{
-    if (strcmp(*it, "Tamachi") == 0)
-    {
-        yamanote.insert(it, "Takanawa Gateway");
-        break;
-    }
-}
-
-printf("2022\n");
-
-for (auto it = yamanote.begin();
-    it != yamanote.end();
-    ++it)
-{
-    printf("%s\n", *it);
-}
-};
-
-
-
-
